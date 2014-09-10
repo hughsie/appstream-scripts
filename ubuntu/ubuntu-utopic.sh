d79dc9c9e02a58f7f22e4a09e6a5439818016b94
Richard Hughes <richard@hughsie.com>
2014-09-10 10:26:21 +0100

    Add helper scripts for Ubuntu

---
+++ ubuntu/ubuntu-utopic.sh
@@ -0,0 +1,10 @@
+time ../../appstream-glib/client/appstream-builder						\
+	--api-version=0.7						\
+	--log-dir=./logs/utopic						\
+	--temp-dir=./tmp						\
+	--cache-dir=../cache-utopic					\
+	--packages-dir=/media/raid/Archive/Ubuntu/			\
+	--extra-appdata-dir=../fedora-appstream/appdata-extra		\
+	--output-dir=./metadata/utopic					\
+	--screenshot-dir=./metadata/utopic				\
+	--basename=ubuntu-utopic
