240ca9e33d6f7ea9e25ace5016b8bccc20ab272d
Richard Hughes <richard@hughsie.com>
2014-09-10 10:25:52 +0100

    Add helper scripts for Arch

---
+++ arch/arch.sh
@@ -0,0 +1,15 @@
+time ../../appstream-glib/client/appstream-builder						\
+	--api-version=0.8						\
+	--add-cache-id							\
+	--verbose							\
+	--max-threads=1							\
+	--old-metadata=./metadata/arch					\
+	--log-dir=./logs						\
+	--temp-dir=./tmp						\
+	--cache-dir=../cache						\
+	--packages-dir=/media/raid/Archive/Arch/packages		\
+	--extra-appdata-dir=../../fedora-appstream/appdata-extra	\
+	--output-dir=./metadata						\
+	--screenshot-dir=./metadata/arch				\
+	--basename=arch							\
+	--screenshot-uri=http://www.archlinux.org/pub/screenshots/
