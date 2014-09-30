f758376246c29fa9750cb291ba7052894a17eced
Richard Hughes <richard@hughsie.com>
2014-09-30 11:37:01 +0100

    HiDPI screenshots

--- fedora/metadata/upload.sh
+++ fedora/metadata/upload.sh
@@ -1,7 +1,9 @@
 chmod a+rx */112x63
-chmod a+rx */112x63
 chmod a+rx */624x351
 chmod a+rx */752x423
+chmod a+rx */224x126
+chmod a+rx */1248x702
+chmod a+rx */1504x846
 chmod a+rx */source
 chmod a+r */*/*.png
 chmod a+r */*.html
