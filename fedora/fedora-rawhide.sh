17dcd8700449db15df0f9c80e11e8056c0962905
Richard Hughes <richard@hughsie.com>
2015-02-25 14:02:57 +0000

    F22 has branched

--- fedora/fedora-rawhide.sh
+++ fedora/fedora-rawhide.sh
@@ -7,35 +7,36 @@ time ${APPSTREAM_GLIB_PATH}/client/appstream-builder			\
 	--verbose							\
 	--add-cache-id							\
 	--min-icon-size=48						\
-	--old-metadata=./metadata/f22					\
 	--enable-hidpi							\
 	--include-failed						\
 	--max-threads=2							\
-	--log-dir=./logs/fedora-22					\
-	--temp-dir=./tmp/fedora-22					\
+	--old-metadata=./metadata/f23					\
+	--log-dir=../../createrepo_as_logs				\
+	--temp-dir=./tmp/fedora-23					\
 	--cache-dir=../cache						\
-	--packages-dir=${ARCHIVE_PATH}/Mirror/Fedora/f22/packages	\
+	--packages-dir=${ARCHIVE_PATH}/Mirror/Fedora/rawhide/packages	\
 	--extra-appstream-dir=${EXTRA_APPSTREAM_PATH}/appstream-extra	\
 	--extra-appdata-dir=${EXTRA_APPSTREAM_PATH}/appdata-extra	\
 	--extra-screenshots-dir=${EXTRA_APPSTREAM_PATH}/screenshots-extra \
-	--output-dir=./metadata/f22					\
-	--screenshot-dir=./metadata/f22					\
-	--basename=fedora-22						\
-	--origin=fedora-22						\
-	--screenshot-uri=http://alt.fedoraproject.org/pub/alt/screenshots/f22/
+	--output-dir=./metadata/f23					\
+	--screenshot-dir=./metadata/f23					\
+	--basename=fedora-23						\
+	--origin=fedora-23						\
+	--screenshot-uri=http://alt.fedoraproject.org/pub/alt/screenshots/f23/
+#exit
 ${APPSTREAM_GLIB_PATH}/client/appstream-util non-package-yaml 		\
-	./metadata/f22/fedora-22.xml.gz					\
-	./metadata/f22/applications-to-import.yaml
+	./metadata/f23/fedora-23.xml.gz					\
+	./metadata/f23/applications-to-import.yaml
 ${APPSTREAM_GLIB_PATH}/client/appstream-util status-html 		\
-	./metadata/f22/fedora-22.xml.gz					\
-	./metadata/f22/status.html
+	./metadata/f23/fedora-23.xml.gz					\
+	./metadata/f23/status.html
 ${APPSTREAM_GLIB_PATH}/client/appstream-util status-html 		\
-	./metadata/f22/fedora-22-failed.xml.gz				\
-	./metadata/f22/failed.html
+	./metadata/f23/fedora-23-failed.xml.gz				\
+	./metadata/f23/failed.html
 ${APPSTREAM_GLIB_PATH}/client/appstream-util matrix-html 		\
-	./metadata/f22/matrix.html					\
-	./metadata/f22/fedora-22.xml.gz					\
-	./metadata/f22/fedora-22-failed.xml.gz
+	./metadata/f23/matrix.html					\
+	./metadata/f23/fedora-23.xml.gz					\
+	./metadata/f23/fedora-23-failed.xml.gz
 
 # sync the screenshots and metadata
 cd metadata/
