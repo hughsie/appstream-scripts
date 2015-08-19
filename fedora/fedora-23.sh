1d5576011f8be147f33dea674b033b05b7cc29bf
Richard Hughes <richard@hughsie.com>
2015-08-19 15:55:43 +0100

    Update F23 to use the new appstream-glib binary

--- fedora/fedora-23.sh
+++ fedora/fedora-23.sh
@@ -1,29 +1,37 @@
-APPSTREAM_GLIB_PATH=../../appstream-glib-fedora
-EXTRA_APPSTREAM_PATH=../../fedora-appstream
+APPSTREAM_GLIB_PATH=../../appstream-glib
 ARCHIVE_PATH=/media/mirror
 
+echo "Building applications..."
 time ${APPSTREAM_GLIB_PATH}/client/appstream-builder			\
 	--api-version=0.8						\
 	--verbose							\
 	--add-cache-id							\
 	--min-icon-size=48						\
-	--old-metadata=./metadata/f23					\
 	--enable-hidpi							\
 	--include-failed						\
-	--max-threads=4							\
+	--max-threads=8							\
+	--old-metadata=./metadata/f23					\
 	--log-dir=./logs/fedora-23					\
 	--temp-dir=./tmp/fedora-23					\
 	--cache-dir=../cache						\
 	--packages-dir=${ARCHIVE_PATH}/Fedora/f23/Packages		\
 	--packages-dir=${ARCHIVE_PATH}/Fedora/f23-updates		\
-	--extra-appstream-dir=${EXTRA_APPSTREAM_PATH}/appstream-extra	\
-	--extra-appdata-dir=${EXTRA_APPSTREAM_PATH}/appdata-extra	\
-	--extra-screenshots-dir=${EXTRA_APPSTREAM_PATH}/screenshots-extra \
 	--output-dir=./metadata/f23					\
-	--screenshot-dir=./metadata/f23					\
 	--basename=fedora-23						\
-	--origin=fedora-23						\
-	--screenshot-uri=http://alt.fedoraproject.org/pub/alt/screenshots/f23/
+	--origin=fedora
+
+echo "Extracting font screenshots"
+cd ./metadata/f23/source
+tar -xvf ../fedora-23-screenshots.tar
+cd -
+
+echo "Mirroring screenshots"
+${APPSTREAM_GLIB_PATH}/client/appstream-util mirror-screenshots		\
+	./metadata/f23/fedora-23.xml.gz					\
+	http://alt.fedoraproject.org/pub/alt/screenshots/f23		\
+	../cache ./metadata/f23
+
+echo "Creating status pages"
 ${APPSTREAM_GLIB_PATH}/client/appstream-util non-package-yaml 		\
 	./metadata/f23/fedora-23.xml.gz					\
 	./metadata/f23/applications-to-import.yaml
@@ -38,7 +46,7 @@ ${APPSTREAM_GLIB_PATH}/client/appstream-util matrix-html 		\
 	./metadata/f23/fedora-23.xml.gz					\
 	./metadata/f23/fedora-23-failed.xml.gz
 
-# sync the screenshots and metadata
+echo "Uploading new metadata"
 cd metadata/
 ./upload.sh
 cd -
